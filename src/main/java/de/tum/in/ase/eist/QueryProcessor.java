d9e3d158e68a9cdf39c650c48a4f50fc4dcd3bbe
Thomas Schuhbeck <ga84xoc@tum.de>
2022-07-13 13:15:18 +0200

    Changed return name

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,6 +13,12 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "thommi";
+        } else if (query.contains("plus")) {
+            //what is 2 plus 0
+            String first = query.substring(8, 1);
+            String second = query.substring(13, 1);
+            int result = Integer.parseInt(first) + Integer.parseInt(second);
+            return String.valueOf(result);
         } else { // TODO extend the programm here
             return "";
         }
